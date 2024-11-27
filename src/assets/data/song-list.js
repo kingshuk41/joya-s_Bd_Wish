f7f59fb5f7768308f9335329af1cfe74265d5b82
kingshuk41 <kingshukpaul1999@gmail.com>
2024-11-28 00:05:39 +0600

    Add files via upload

---
+++ src/assets/data/song-list.js
@@ -0,0 +1,116 @@
+// 1:
+import Song1 from '../audio/song1.mp3';
+import Img1 from '../img/png/img6.png';
+// 2:
+import Song2 from '../audio/song2.mp3';
+import Img2 from '../img/jpg/img7.jpg';
+// 3:
+import Song3 from '../audio/song3.mp3';
+import Img3 from '../img/png/img5.png';
+// 4:
+import Song4 from '../audio/song4.mp3';
+import Img4 from '../img/png/img4.png';
+// 5:
+import Song5 from '../audio/song5.mp3';
+import Img5 from '../img/png/img8.png';
+// 6:
+import Song6 from '../audio/song6.mp3';
+import Img6 from '../img/jpg/img9.jpg';
+// 7:
+import Song7 from '../audio/song7.mp3';
+import Img7 from '../img/jpg/img10.jpg';
+// 8:
+import Song8 from '../audio/song8.mp3';
+import Img8 from '../img/jpg/img11.jpg';
+// 9:
+import Song9 from '../audio/song9.mp3';
+import Img9 from '../img/jpeg/img2.jpeg';
+// 10:
+import Song10 from '../audio/song10.mp3';
+import Img10 from '../img/jpg/img12.jpg';
+
+const songList = [
+    {
+        id: '1',
+        song: Song1,
+        img: Img1,
+        title: "Tu Hi Hai  Half Girlfriend",
+        p: 'Believe in yourself and all that you are. (REELIFY DEVOLOPED BY KINGSHUK)',
+        yt: 'https://www.youtube.com/watch?v=KyMNLIYRsR4'
+    },
+    {
+        id: '2',
+        song: Song2,
+        img: Img2,
+        title: "Deewana Kar Raha Hai • by Javed Ali",
+        p: "Your journey may not always be easy, but every step forward is progress. Keep going!",
+
+        yt: 'https://youtu.be/3O6eYd8pUM8?si=esGjpZUKRocTCUQC'
+    },
+    {
+        id: '3',
+        song: Song3,
+        img: Img3,
+        title: "Shayad • Arijit Singh",
+        p: 'The only limit to your success is the one you place on yourself. Dream big and go for it!',
+        yt: 'https://youtu.be/Z0M_t52MDpo?si=VpsFi7rb2srDygcX'
+    },
+    {
+        id: '4',
+        song: Song4,
+        img: Img4,
+        title: "Mann Mast Magan • by Arijit Singh",
+        p: "When you feel like giving up, remember why you started.",
+        yt: 'https://youtu.be/fXRvluHnjxE?si=_y1wkPg0bsPJuNR8'
+    },
+    {
+        id: '5',
+        song: Song5,
+        img: Img5,
+        title: "Ishq Sufiyana • by Kamal Khan",
+        p: "You have the power to create your own destiny. Take the first step today.",
+        yt: 'https://youtu.be/5RT6QMKJTjQ?si=6A6OlTzp9hfxmn7o'
+    },
+    {
+        id: '6',
+        song: Song6,
+        img: Img6,
+        title: "LAAL ISHQ • Deepika Padukone & Ranveer Singh",
+        p: "Success doesn’t come from what you do occasionally; it comes from what you do consistently.",
+        yt: 'https://www.youtube.com/watch?v=PycY_BqbWaw'
+    },
+    {
+        id: '7',
+        song: Song7,
+        img: Img7,
+        title: "Piya Aye Na • by KK",
+        p: "Every challenge you face is an opportunity to grow stronger. Embrace it!",
+        yt: 'https://youtu.be/2bMEe0UYa8E?si=rQtEzmcngIIeZuLF'
+    },
+    {
+        id: '8',
+        song: Song8,
+        img: Img8,
+        title: "Laboon Ko • by KK",
+        p: "Don't wait for the perfect moment. Take the moment and make it perfect.",
+        yt: 'https://youtu.be/0OGguI0uDfE?si=iJuZVtftoHKzYPP2'
+    },
+    {
+        id: '9',
+        song: Song9,
+        img: Img9,
+        title: "Khuda Jane • by KK",
+        p: "The future belongs to those who believe in the beauty of their dreams.",
+        yt: 'https://youtu.be/RJD1fQ15I9Y?si=C446h4kLaWnhEXVM'
+    },
+    {
+        id: '10',
+        song: Song10,
+        img: Img10,
+        title: "tu hi din tu hi meri • by Jubin Nautiyal",
+        p: "You are capable of amazing things. Never doubt your potential.",
+        yt: 'https://www.youtube.com/watch?v=Ojh9GGpLxl8'
+    },
+];
+
+export default songList;
